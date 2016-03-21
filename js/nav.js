8baaac9d5f1f779bdb90fae3b22150218c9513f6
Matt Cassara <cassaram09@gmail.com>
2016-03-21 00:07:26 -0400

    fixed navigation menu, added anchor scroll, reorganized home page, worked on dallas grid for index, changed color palette,

--- js/nav.js
+++ js/nav.js
@@ -2,3 +2,9 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+$('.nav-item').click(function() {
+$('.button_container').toggleClass('active');
+   $('#overlay').toggleClass('open');
+
+  });
